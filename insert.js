0a5d96ed4b9ef1abfb74bb270cadbf77d1d4e21a
TheCodeWorm <4purposeliving@gmail.com>
2018-06-20 12:18:14 -0700

    insert data

---
+++ insert.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'articles'
+});
+connection.connect();
+
+/*
+// insert following article
+const article = {
+	author: 'Noel Caceres',
+	title: 'Node Mysql',
+	body: 'foo bar'
+};
+
+// insert data
+let query = connection.query('insert into articles set ?', article, function(err, result) {
+  if (err) {
+  	console.log(err);
+  	return;
+  }
+  console.log(query.sql);
+});
+*/
